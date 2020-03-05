f6a0c6611b86460a47c8b9e098dc9771570d01a6
VihaanAkshaay <akswins001@gmail.com>
2020-03-06 02:10:59 +0530

    Added files with simple_arm simulation in gazebo and controlled the joint parameters

---
+++ catkin_ws/build/iBot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vihaan/PycharmProjects/BasicROSProject/catkin_ws/src/iBot
+# Build directory: /home/vihaan/PycharmProjects/BasicROSProject/catkin_ws/build/iBot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
